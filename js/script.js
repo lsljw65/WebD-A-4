a3e703bfe87fe7584001c89c2b076db5281535bf
lee <lsljw65@gmail.com>
2024-08-17 14:45:08 +0900

    서브메뉴 드롭다운 구현

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-$(function () {});
+$(function () {
+  // 서브메뉴
+  $(".sub-menu").each(function (index) {
+    $(this).mouseenter(function () {
+      $("nav>ul>li>a").removeClass("active");
+      $("nav>ul>li>a").eq(index).addClass("active");
+    });
+    $(this).mouseleave(function () {
+      $("nav>ul>li>a").removeClass("active");
+    });
+  });
+  $("nav").mouseenter(function () {
+    $(".sub-back").stop().slideDown();
+    $(".sub-menu").stop().slideDown();
+  });
+  $("nav").mouseleave(function () {
+    $(".sub-back").stop().slideUp();
+    $(".sub-menu").stop().slideUp();
+  });
+});
