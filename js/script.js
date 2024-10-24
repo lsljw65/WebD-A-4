5190caa1fa4c5f2b6f5086140eee1a7a5ff8d79d
lee <lsljw65@gmail.com>
2024-10-24 15:03:24 +0900

    슬라이더 fade 구현

--- js/script.js
+++ js/script.js
@@ -17,4 +17,14 @@ $(function () {
     $(".sub-back").stop().slideUp();
     $(".sub-menu").stop().slideUp();
   });
+  //   슬라이드
+  $(".sliders>li:gt(0)").hide();
+  setInterval(function () {
+    $(".sliders>li:first-child")
+      .fadeOut()
+      .next()
+      .fadeIn()
+      .end()
+      .appendTo(".sliders");
+  }, 3000);
 });
